b1ae3f8fb650b681901c34621795259fea53ecae
Priyanshuomer <priyanshuomer27@gmail.com>
2025-07-04 10:46:53 +0530

    add admin plan controllers

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { isAdmin } from "../middleware/role.js";
 
-import {introducePlan, blockPlan, updatePlan} from "../controllers/Admins/planController.js";
+import {introducePlan, blockPlan, updatePlan, activateBlockedPlan} from "../controllers/Admins/planController.js";
 import {introduceProduct, blockProduct, updateProduct} from "../controllers/Admins/productController.js";
 import {allUsers, blockUser} from "../controllers/Admins/userController.js";
 
@@ -18,6 +18,7 @@ router.put("/update-product/:productID", verifyToken, isAdmin, updateProduct);
 router.post("/add-plan", verifyToken, isAdmin,
 introducePlan);
 router.post("/block-plan/:planID", verifyToken, isAdmin, blockPlan);
+router.post("/activate-plan/:planID", verifyToken, isAdmin, activateBlockedPlan);
 router.put("/update-plan/:planID", verifyToken, isAdmin, updatePlan);
 
 export default router;

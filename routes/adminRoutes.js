00710ff2e0e2e1adc7a7ef590085e9e51de572e0
TusharS0012 <tushardestro17@gmail.com>
2025-07-04 13:40:30 +0530

    Refactor product controller and update admin routes to include viewAllProducts functionality

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,8 +3,9 @@ import { verifyToken } from "../middleware/verifyToken.js";
 import { isAdmin } from "../middleware/role.js";
 
 import {introducePlan, blockPlan, updatePlan} from "../controllers/Admins/planController.js";
-import {introduceProduct, blockProduct, updateProduct} from "../controllers/Admins/productController.js";
+import {introduceProduct, blockProduct, updateProduct , viewAllProducts} from "../controllers/Admins/productController.js";
 import {allUsers, blockUser} from "../controllers/Admins/userController.js";
+import { viewAllAvailableProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
@@ -14,10 +15,8 @@ router.post("/block-user/:userID", verifyToken, isAdmin, blockUser);
 router.post("/add-product", verifyToken, isAdmin, introduceProduct);
 router.post("/block-product/:productID", verifyToken, isAdmin, blockProduct);
 router.put("/update-product/:productID", verifyToken, isAdmin, updateProduct);
+router.get("/products", verifyToken, isAdmin, viewAllProducts);
 
-router.post("/add-plan", verifyToken, isAdmin,
-introducePlan);
+router.post("/add-plan", verifyToken, isAdmin, introducePlan);
 router.post("/block-plan/:planID", verifyToken, isAdmin, blockPlan);
 router.put("/update-plan/:planID", verifyToken, isAdmin, updatePlan);
-
-export default router;

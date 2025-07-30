f1b875fbb27a1e1284133bb2457809422830fcc5
Priyanshuomer <priyanshuomer27@gmail.com>
2025-07-30 21:10:18 +0530

    add all and active plan route  controller

--- controllers/planController.js
+++ controllers/planController.js
@@ -1,4 +1,5 @@
 import Plan from "../models/Plan.js";
+import Subscription from "../models/Subscription.js";
 
 
 export const viewAllAvailablePlans = async (req,res) => {
@@ -17,4 +18,53 @@ export const addPlanToUser = async (req,res) => {
 
 export const subscribedPlansOfUser = async (req,res) => {
 
+};
+
+
+export const allActivePlansOfUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const subscriptions = await Subscription.find({
+      user: userId,
+      status: "active",
+    })
+      .populate("Plan")
+      .populate("Product");
+
+    res.status(200).json({
+      success: true,
+      data: subscriptions,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch active plans",
+      error: error.message,
+    });
+  }
+};
+
+
+export const allPlansOfUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const subscriptions = await Subscription.find({
+      user: userId
+    })
+      .populate("Plan")
+      .populate("Product");
+
+    res.status(200).json({
+      success: true,
+      data: subscriptions,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch active plans",
+      error: error.message,
+    });
+  }
 };
--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { viewPlanById , viewAllAvailablePlans} from "../controllers/planController.js";
+import { viewPlanById , viewAllAvailablePlans, allActivePlansOfUser, allPlansOfUser} from "../controllers/planController.js";
 import { viewAllAvailableProducts , viewProductById} from "../controllers/productController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -12,5 +12,10 @@ router.get("/view-allPlans", verifyToken, viewAllAvailablePlans);
 router.get("/view-product", verifyToken, viewProductById);
 
 router.get("/view-allProducts", verifyToken, viewAllAvailableProducts);
+
+router.get("all-active-plans/:userId",verifyToken,allActivePlansOfUser);
+
+router.get("all-plans/:userId",verifyToken,allPlansOfUser);
+
 
 export default router;

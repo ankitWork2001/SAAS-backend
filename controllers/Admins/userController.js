648d913e99316fc67929e5c4200b929e4b561022
TusharS0012 <tushardestro17@gmail.com>
2025-07-30 05:53:25 +0530

    add dashboard view and enhance token verification logic

--- controllers/Admins/userController.js
+++ controllers/Admins/userController.js
@@ -1,5 +1,5 @@
 import User from "../../models/User.js";
-
+import Subscription from "../../models/Subscription.js";
 export const allUsers = async (req, res) => {
   try {
     const users = await User.find({});
@@ -57,5 +57,85 @@ export const activateBlockedUser = async (req, res) => {
     res
       .status(500)
       .json({ success: false, message: "Server error.", error: error.message });
+  }
+};
+
+export const dashboardView = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user.role === "ownAdmin")
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+
+    const totalUsers = await User.countDocuments();
+    const totalSubscriptions = await Subscription.countDocuments();
+    const recentUsers = await User.find().sort({ createdAt: -1 }).limit(5);
+    const totalRevenueResult = await Subscription.aggregate([
+      {
+        $match: { status: "active" },
+      },
+      {
+        $lookup: {
+          from: "plans",
+          localField: "plan",
+          foreignField: "_id",
+          as: "planDetails",
+        },
+      },
+      {
+        $unwind: "$planDetails",
+      },
+      {
+        $addFields: {
+          durationInDays: {
+            $divide: [
+              { $subtract: ["$endDate", "$startDate"] },
+              1000 * 60 * 60 * 24,
+            ],
+          },
+        },
+      },
+      {
+        $addFields: {
+          effectivePrice: {
+            $cond: {
+              if: {
+                $and: [
+                  { $ne: ["$planDetails.priceYearly", null] },
+                  { $ne: ["$planDetails.priceYearly", undefined] },
+                  { $gte: ["$durationInDays", 360] },
+                ],
+              },
+              then: "$planDetails.priceYearly",
+              else: "$planDetails.priceMonthly",
+            },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalAmount: {
+            $sum: "$effectivePrice",
+          },
+        },
+      },
+    ]);
+
+    const totalSubscriptionValue =
+      totalRevenueResult.length > 0 ? totalRevenueResult[0].totalAmount : 0;
+
+    res.status(200).json({
+      success: true,
+      totalUsers,
+      totalSubscriptions,
+      totalrevenue: totalSubscriptionValue,
+      recentUsers,
+      // user,
+    });
+  } catch (err) {
+    console.error("Dashboard View Error:", err.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Server error", error: err.message });
   }
 };
